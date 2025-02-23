c6e6cc218eb9c506792487328153adde8751e568
Elena <elena@192.168.10.4>
2025-02-23 16:08:19 +0700

    change port

--- index.js
+++ index.js
@@ -126,6 +126,6 @@ app.post('/web-data', async (req, res) => {
 });
 
 
-const PORT = 8010;
+const PORT = 8020;
 app.listen(PORT, '0.0.0.0', () => console.log(`[SERVER] Сервер запущен на порту ${PORT}`));
 
