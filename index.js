c111e653b9e0e0e77c7a3331299881087db0791f
ElenaSoshina <soshinaalena6415@gmail.com>
2025-03-03 19:53:50 +0700

    jdhf

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ bot.on('message', async (msg) => {
 });
 
 app.post('/web-data', async (req, res) => {
+    console.log('Получены данные:', req.body);
     const {queryId, products = [], totalPrice} = req.body;
     try {
         await bot.answerWebAppQuery(queryId, {
